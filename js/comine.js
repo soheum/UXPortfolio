ad6c7d68daa787d81e006508aec9f18b1a79b460
soheum <soheum@naver.com>
2020-02-27 06:29:05 +0100

    comine password protect

--- js/comine.js
+++ js/comine.js
@@ -1,4 +1,19 @@
+var s ="";
 
+while (s!= "hellosoheum")
+{
+s=prompt("please enter your password");
+if (s=="hellosoheum")
+{
+window.location.href="file:///Users/hwangsoheum/Documents/ciidfinal/comine2.html"; //page to redirect if password entered is correct
+
+}
+else
+{
+alert("Incorrect password-Try again");
+
+}
+}
 $(document).ready(function(){
 
 
