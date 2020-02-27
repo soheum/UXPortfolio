e06742e6d5f852aa49fd149bf70b0b41fa5cdb8a
soheum <soheum@naver.com>
2020-02-27 06:30:57 +0100

    comine password2

--- js/comine.js
+++ js/comine.js
@@ -5,7 +5,7 @@ while (s!= "hellosoheum")
 s=prompt("please enter your password");
 if (s=="hellosoheum")
 {
-window.location.href="file:///Users/hwangsoheum/Documents/ciidfinal/comine2.html"; //page to redirect if password entered is correct
+window.location.href="https://sohheum.com/comine.html"; //page to redirect if password entered is correct
 
 }
 else
