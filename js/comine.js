79e2f4c237b71663014431dc76f81093277da827
soheum <soheum@naver.com>
2020-02-28 09:56:01 +0100

    comine password_undo

--- js/comine.js
+++ js/comine.js
@@ -1,19 +1,19 @@
-var s ="";
-
-while (s!= "hellosoheum")
-{
-s=prompt("please enter your password");
-if (s=="hellosoheum")
-{
-window.location.href="https://sohheum.com/comine.html"; //page to redirect if password entered is correct
-
-}
-else
-{
-alert("Incorrect password-Try again");
-
-}
-}
+// var s ="";
+//
+// while (s!= "hellosoheum")
+// {
+// s=prompt("please enter your password");
+// if (s=="hellosoheum")
+// {
+// window.location.href="https://sohheum.com/comine2.html"; //page to redirect if password entered is correct
+//
+// }
+// else
+// {
+// alert("Incorrect password-Try again");
+//
+// }
+// }
 $(document).ready(function(){
 
 
